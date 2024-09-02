ed012ae7d2e8848eb3c7744fe5faee383213d311
julio orozco <julio11orozco09gmail.com>
2024-09-01 23:18:30 -0600

    funcionalidad de division

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ def resta(num1: int, num2: int):
     return num1 - num2
 
 print(resta(num1 = 20, num2 = 10))
+
+# funcion de division:
+def division(num1: int, num2: int):
+    if num2 != 0:
+        return num1 / num2
+    else:
+        return "Error, no se puede dividir entre cero"
+
+print(division(num1 = 10, num2 = 2))
